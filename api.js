3a73c35f4afa394ce057a09ed788b4e40356c537
Utkarsh Jain <utkarsh57917@gmail.com>
2020-06-06 11:49:51 +0530

    test 2

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ const api_url = 'https://api.covid19api.com/summary';
 async function getdata(){
     const response = await fetch(api_url);
     const data = await response.json();
-    //console.log(data);test
+    //console.log(data);test2
     document.getElementById('totca').textContent = data.Global.TotalConfirmed;
     document.getElementById('totde').textContent = data.Global.TotalDeaths;
     document.getElementById('totre').textContent = data.Global.TotalRecovered;
