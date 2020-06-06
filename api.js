e6ac9524fd256b15ba37fc5fcf901ce482ae5dcd
Utkarsh Jain <utkarsh57917@gmail.com>
2020-06-07 00:16:33 +0530

    statewise

--- api.js
+++ api.js
@@ -1,10 +1,53 @@
-const api_url = 'https://api.covid19api.com/summary';
+const api_url = 'https://api.covid19india.org/data.json';
 async function getdata(){
     const response = await fetch(api_url);
     const data = await response.json();
     //console.log(data);test2
-    document.getElementById('totca').textContent = data.Global.TotalConfirmed;
-    document.getElementById('totde').textContent = data.Global.TotalDeaths;
-    document.getElementById('totre').textContent = data.Global.TotalRecovered;
+    document.getElementById('totac').textContent = data.statewise[0].active;
+    document.getElementById('totca').textContent = data.statewise[0].confirmed;
+    document.getElementById('totde').textContent = data.statewise[0].deaths;
+    document.getElementById('totre').textContent = data.statewise[0].recovered;
+    
+    var col = [];
+        for (var i = 0; i < data.statewise.length; i++) {
+            for (var key in data.statewise[i]) {
+                 if (key == "active"||key == "confirmed"||key == "deaths"||key == "recovered"||key =="state") {
+                        if (col.indexOf(key) === -1) {
+                        col.push(key);
+                        console.log(key);
+                   }
+                }
+            }
+        }
+        console.log(col);
+        // Create a table.
+        var table = document.createElement("table");
+
+        // Create table header row using the extracted headers above.
+        var tr = table.insertRow(-1);                   // table row.
+
+        for (var i = 0; i < col.length; i++) {
+            var th = document.createElement("th");      // table header.
+            th.innerHTML = col[i];
+            tr.appendChild(th);
+        }
+
+        // add json data to the table as rows.
+        for (var i = 0; i < data.statewise.length; i++) {
+
+            tr = table.insertRow(-1);
+
+            for (var j = 0; j < col.length; j++) {
+                var tabCell = tr.insertCell(-1);
+                tabCell.innerHTML = data.statewise[i][col[j]];
+            }
+        }
+
+        // Now, add the newly created table with json data, to a container.
+        var divShowData = document.getElementById('showData');
+        divShowData.innerHTML = "";
+        divShowData.appendChild(table);
+        
+    
 }
 getdata();
